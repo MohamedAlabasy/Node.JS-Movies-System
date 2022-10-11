78a8bcbfa2a42023f7b7b42a53774812f9855a80
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-10-11 16:12:47 +0200

    update users table

--- migrations/sqls/20221011140115-create-users-table-up.sql
+++ migrations/sqls/20221011140115-create-users-table-up.sql
@@ -3,6 +3,8 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     email VARCHAR(255) UNIQUE NOT NULL,
     name VARCHAR(255) ,
+    is_verification BOOLEAN DEFAULT FALSE,
     password VARCHAR NOT NULL,
-    is_verification BOOLEAN DEFAULT FALSE
+    birthday timestamp NOT NULL,
+    is_admin BOOLEAN DEFAULT FALSE
 );
